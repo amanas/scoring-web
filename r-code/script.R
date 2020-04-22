787a2b743ac64da0b174c754b9471a7a08191929
amanas <andresmanasmanas@gmail.com>
2020-04-22 10:13:49 +0200

    go

--- r-code/script.R
+++ r-code/script.R
@@ -13,14 +13,31 @@ print(args)
 print("Loading the library")
 library(bigrquery)
 
-print("Authenticating")
-bq_auth()
 
-print("Querying")
-projectid <- args[1]
-table <- args[2]
-sql <- paste("select * from", table)
-df <- query_exec(sql, projectid, use_legacy_sql = FALSE)
+tryCatch(
+    {            
+        print("Authenticating")
+        bq_auth()
+        print("Querying")
+        projectid <- args[1]
+        table <- args[2]
+        sql <- paste("select * from", table)
+        df <- query_exec(sql, projectid, use_legacy_sql = FALSE)
+        print("Showing result")
+        df
+    },
+    error=function(cond) {
+        message("Error:")
+        message(cond)
+        return(NA)
+    },
+    warning=function(cond) {
+        message("Warning:")
+        message(cond)
+        return(NULL)
+    },
+    finally={
+        message("OK")
+    }
+)  
 
-print("Showing result")
-df
--- r-runner/cloudbuild.sh
+++ r-runner/cloudbuild.sh
@@ -11,12 +11,19 @@ gcloud compute instances create-with-container $VM_NAME \
     --container-image $IMAGE \
     --container-arg="greetings.R" \
     --container-arg="$PROJECT_ID" \
-    --container-arg="negocio.scoring-web"
+    --container-arg="negocio.scoring_web" \
+    --scopes=default,bigquery
 
 
+        "https://www.googleapis.com/auth/devstorage.read_only",
+        "https://www.googleapis.com/auth/logging.write",
+        "https://www.googleapis.com/auth/monitoring.write",
+        "https://www.googleapis.com/auth/servicecontrol",
+        "https://www.googleapis.com/auth/service.management.readonly",
+        "https://www.googleapis.com/auth/trace.append"
 
-    --service-account=model-runner@qwiklabs-gcp-00-ff78fb12d597.iam.gserviceaccount.com \
-    --scopes=https://www.googleapis.com/auth/bigquery,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/trace.append,https://www.googleapis.com/auth/devstorage.read_only \    
+--service-account=model-runner@qwiklabs-gcp-00-ff78fb12d597.iam.gserviceaccount.com \
+--scopes=https://www.googleapis.com/auth/bigquery,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/trace.append,https://www.googleapis.com/auth/devstorage.read_only \    
 
 
 gcloud beta auth configure-docker europe-west1-docker.pkg.dev
