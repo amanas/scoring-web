3b55bd9cd4a86f129472401be414144b48750ea1
amanas <andresmanasmanas@gmail.com>
2020-04-22 20:20:48 +0200

    go

--- image-r-runner/cloudbuild-via-image.sh
+++ image-r-runner/cloudbuild-via-image.sh
@@ -5,7 +5,7 @@ PARAM_SCRIPT="script.R"
 PARAM_PROJECT_ID=$DEVSHELL_PROJECT_ID
 PARAM_DATASET_TABLE="negocio.scoring_web"
 IMG_NAME="r-runtime"
-VM_NAME="runner4-$PARAM_REPO"
+VM_NAME="runner4-$PARAM_REPO"-$(date  +%Y%m%d%H%M%S)
 ZONE="europe-west1-b"
 
 gcloud beta compute instances create $VM_NAME \
@@ -14,14 +14,20 @@ gcloud beta compute instances create $VM_NAME \
     --scopes=default,bigquery,cloud-source-repos-ro \
     --metadata startup-script="#! /bin/bash
 
+mkdir /working
+cd /working
+
 export PARAM_REPO=$PARAM_REPO
 export PARAM_SCRIPT=$PARAM_SCRIPT
 export PARAM_PROJECT_ID=$PARAM_PROJECT_ID
 export PARAM_DATASET_TABLE=$PARAM_DATASET_TABLE
 
-gcloud source repos clone $PARAM_REPO --project=$PARAM_PROJECT_ID
+echo HOLA_1 >>log
+sudo gcloud source repos clone $PARAM_REPO --project=$PARAM_PROJECT_ID >>log 2>&1
+echo HOLA_2 >>log
 cd $PARAM_REPO
-Rscript --vanilla $PARAM_SCRIPT $PARAM_PROJECT_ID $PARAM_DATASET_TABLE
+sudo Rscript --vanilla $PARAM_SCRIPT $PARAM_PROJECT_ID $PARAM_DATASET_TABLE  >>log 2>&1
+echo HOLA_3 >>log
 "
 
 # gcloud source repos clone scoring-web --project=qwiklabs-gcp-03-2da9555533b6
--- image-r-runtime/cloudbuild.sh
+++ image-r-runtime/cloudbuild.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
 IMG_NAME="r-runtime"
-VM_NAME="r-model"
+VM_NAME="$IMAGE_NAME-$(date  +%Y%m%d%H%M%S)"
 ZONE="europe-west1-b"
 
-gcloud compute instances create $IMG_NAME \
+gcloud compute instances create $VM_NAME \
     --zone $ZONE \
     --scopes=default,bigquery \
     --metadata-from-file startup-script=setup_r_runtime.sh
 
 gcloud beta compute machine-images create $IMG_NAME \
-    --source-instance $IMG_NAME \
+    --source-instance $VM_NAME \
     --source-instance-zone $ZONE
 
-gcloud compute instances delete $IMG_NAME --zone $ZONE --quiet    
+gcloud compute instances delete $VM_NAME --zone $ZONE --quiet    
